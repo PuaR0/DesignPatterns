1b181cbb92d56f3bb5fb69b33db41c37c4569dea
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-30 11:15:43 +0300

    Initial Commit

---
+++ DesignPatterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPatterns
+//
+//  Created by Алиага С on 30.6.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
